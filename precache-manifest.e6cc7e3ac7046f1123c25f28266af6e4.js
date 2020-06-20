4245e88a3ee7baa56f8c02c800c7f7420dede84c
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 06:37:08 +0300

    Updates

---
+++ precache-manifest.e6cc7e3ac7046f1123c25f28266af6e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea4c3d1b659a527a9f837e4a3f9bfb4",
+    "url": "/zero-points/index.html"
+  },
+  {
+    "revision": "9e2266c068ff26c05a20",
+    "url": "/zero-points/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba653f549180d49b05ef",
+    "url": "/zero-points/static/js/2.45ccdb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zero-points/static/js/2.45ccdb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2266c068ff26c05a20",
+    "url": "/zero-points/static/js/main.07c0002a.chunk.js"
+  },
+  {
+    "revision": "a29be22535d9dd4c871a",
+    "url": "/zero-points/static/js/runtime-main.4945b843.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zero-points/static/media/logo.5d5d9eef.svg"
+  }
+]);
